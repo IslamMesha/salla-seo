b74c703c957bf052fbdd490a71acd293618df434
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-03-05 19:35:40 +0200

    create endpoint for test everything is fine

--- dj/settings.py
+++ dj/settings.py
@@ -25,6 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # apps
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -35,9 +38,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    # apps
-    'app',
 ]
 
 ROOT_URLCONF = 'dj.urls'
