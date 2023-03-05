1e466bc49147dd6e4e5c7d84112f90335b4156b3
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-03-05 19:39:31 +0200

    setup templates

--- dj/settings.py
+++ dj/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     # apps
     'app',
 ]
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'dj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
