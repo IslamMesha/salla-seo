e5ed952755059ec9c3db1a814cbc3764e5774829
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-03-07 21:19:52 +0200

    install dot env package

--- dj/settings.py
+++ dj/settings.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(f'{BASE_DIR}/.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
