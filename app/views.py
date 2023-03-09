4ab07c5dc3dab10d88671c10d85f957e0bea0c76
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-03-09 02:25:19 +0200

    create test view

--- app/views.py
+++ app/views.py
@@ -27,5 +27,10 @@ def oauth_callback(request):
 
     return redirect('app:index')
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+class Test(APIView):
+    def get(self, request):
+        return Response({'message': 'Hello World!'})
 
