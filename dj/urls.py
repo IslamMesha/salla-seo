9687aa0ef814f381731c6067cd8b25aa8230486a
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-03-09 02:24:59 +0200

    register and organize urls

--- dj/urls.py
+++ dj/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # apps
     path('', include('app.urls', namespace='app')),
+
+    # third party endpoints
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 ]
