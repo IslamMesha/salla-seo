fcc1c0e4f5137b9755dbe4e149f4c2757317cf7d
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-03-09 02:46:45 +0200

    Create auth class needed to identify the salla account

---
+++ app/authentication.py
@@ -0,0 +1,37 @@
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import exceptions
+
+from app.models import Account
+
+
+class TokenAuthSupportCookie(TokenAuthentication):
+    """
+    Extend the TokenAuthentication class to support cookie based authentication
+    """
+    model = Account
+
+    def authenticate_credentials(self, key):
+        model = self.get_model()
+        try:
+            token = model.objects.get(public_token=key)
+        except model.DoesNotExist:
+            raise exceptions.AuthenticationFailed(_('Invalid token.'))
+
+        if not token.is_active:
+            raise exceptions.AuthenticationFailed(_('User inactive or deleted.'))
+
+        return (token.user, token)
+
+    def authenticate(self, request):
+        is_auth_by_header = 'HTTP_AUTHORIZATION' in request.META
+        is_auth_by_cookie = 'auth_token' in request.COOKIES
+
+        if is_auth_by_header:
+            result = super().authenticate(request)
+        if is_auth_by_cookie:
+            token = request.COOKIES.get('auth_token')
+            result = self.authenticate_credentials(token)
+
+        return result
