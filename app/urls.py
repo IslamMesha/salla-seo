741a4d41adc4f30f4e643c840116c6d46f5ecbc1
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-03-09 03:06:06 +0200

    wrong namesapce

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,6 @@ app_name = 'app'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('test/', views.Test.as_view(), name='index'),
+    path('test/', views.Test.as_view(), name='test'),
     path('oauth/callback/', views.oauth_callback, name='oauth_callback'),
 ]
