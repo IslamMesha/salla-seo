2e0386d38fbfc378854f3fd156880d449b9a468f
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-03-09 03:04:45 +0200

    set cookie after login

--- app/views.py
+++ app/views.py
@@ -3,9 +3,13 @@ import os
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 from app.controllers import SallaOAuth
 from app.exceptions import SallaOauthFailedException
 from app.models import Account
+from app.utils import set_cookie
 
 
 def index(request):
@@ -13,6 +17,7 @@ def index(request):
     context = {
         'installation_url': f'https://s.salla.sa/apps/install/{app_id}',
     }
+    print(Account.objects.filter(public_token=request.COOKIES.get('auth_token')))
     return render(request, 'index.html', context=context)
 
 
@@ -21,14 +26,14 @@ def oauth_callback(request):
     if code:
         messages.success(request, 'Login Success.')
         data = SallaOAuth().get_access_token(code)
-        Account.store(data)
+        account = Account.store(data)
+        response = redirect('app:index')
+        
+        set_cookie(response, 'auth_token', account.public_token, max_age=60 * 60 * 24 * 14)
     else:
         raise SallaOauthFailedException()
 
-    return redirect('app:index')
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
+    return response
 
 class Test(APIView):
     def get(self, request):
