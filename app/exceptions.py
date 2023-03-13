1a5d600b992c5477a5443675f7aeedc101872cd2
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-03-13 06:29:20 +0200

    create new exception to show error happened from where

--- app/exceptions.py
+++ app/exceptions.py
@@ -8,3 +8,9 @@ class SallaOauthFailedException(APIException):
     default_code = 'error'
     status_code = status.HTTP_401_UNAUTHORIZED
 
+
+class SallaEndpointFailureException(APIException):
+    default_detail = 'Salla Oauth Failed.'
+    default_code = 'error'
+    status_code = status.HTTP_424_FAILED_DEPENDENCY
+
