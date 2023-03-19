11bf8f261819ab3b6cc9e157cd7ebab86a0eb797
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-03-20 01:38:24 +0200

    serializer that validate and save responses log into db

--- app/serializers.py
+++ app/serializers.py
@@ -35,3 +35,24 @@ class ProductEndpointParamsSerializer(serializers.Serializer):
     per_page = serializers.IntegerField(required=False, default=10)
     status = serializers.ChoiceField(required=False, choices=STATUS_CHOICES)
 
+
+class ChatGPTResponseSerializer(serializers.Serializer):
+    id = serializers.CharField(write_only=True, required=False)
+    object = serializers.CharField(write_only=True, required=False)
+    created = serializers.CharField(write_only=True, required=False)
+    model = serializers.CharField(write_only=True, required=False)
+    usage = serializers.DictField(write_only=True)
+    choices = serializers.ListField(write_only=True, child=serializers.DictField())
+
+    prompt = serializers.CharField()
+
+    total_tokens = serializers.IntegerField(source='usage.total_tokens', read_only=True)
+    answer = serializers.SerializerMethodField(read_only=True)
+    full_response = serializers.DictField(source='*', read_only=True)
+
+    def get_answer(self, obj):
+        return obj['choices'][0]['text'].strip()
+
+    def save(self, **kwargs):
+        return models.ChatGPTLog.objects.create(**self.data)
+
