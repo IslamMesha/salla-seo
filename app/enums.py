cc91bc681eaaaa736692be3815ad8ffb58c21f56
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-03-20 03:14:38 +0200

    create templates that may help me creating chat gpt prompt

--- app/enums.py
+++ app/enums.py
@@ -5,3 +5,9 @@ class CookieKeys(Enum):
     AUTH_TOKEN = 'auth_token'
 
 
+class ChatGPTPromptTemplates(Enum):
+    # hold templates for asking chatgpt
+    PRODUCT_DESCRIPTION_EN = 'write a brief about product {product_name}'
+    PRODUCT_DESCRIPTION_AR = 'اكتب ملخصاً عن المنتج {product_name}'
+
+
