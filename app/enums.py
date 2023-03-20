e0d8daecc2df1753cbe98e0ed7d533c8d6897855
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-03-20 23:55:49 +0200

    move template another place

--- app/enums.py
+++ app/enums.py
@@ -5,9 +5,3 @@ class CookieKeys(Enum):
     AUTH_TOKEN = 'auth_token'
 
 
-class ChatGPTPromptTemplates(Enum):
-    # hold templates for asking chatgpt
-    PRODUCT_DESCRIPTION_EN = 'write a brief about product {product_name}'
-    PRODUCT_DESCRIPTION_AR = 'اكتب ملخصاً عن المنتج {product_name}'
-
-
