498191a07ff9f5291ee85333650c9c14678a9fdc
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-03-22 02:38:45 +0200

    register logs models to admin

--- app/admin.py
+++ app/admin.py
@@ -5,4 +5,6 @@ from app import models
 admin.site.register(models.SallaUser)
 admin.site.register(models.SallaStore)
 admin.site.register(models.Account)
+admin.site.register(models.ChatGPTLog)
+admin.site.register(models.UserPrompt)
 
