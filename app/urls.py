74832766513e14bf39f58960bbe51936d8a49587
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-03-29 06:31:39 +0200

    raname endpoints to be wore described

--- app/urls.py
+++ app/urls.py
@@ -7,8 +7,10 @@ app_name = 'app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.ProductsListAPI.as_view(), name='list_products'),
-    path('product/description/', views.ProductGetDescriptionAPI.as_view(), name='ask_for_description'),
-    path('product/description/<str:product_id>/', views.ProductListDescriptionsAPI.as_view(), name='list_descriptions'),
+
+    path('ask-gpt/description/', views.ProductGetDescriptionAPI.as_view(), name='ask_for_description'),
+    path('salla/write-description/<str:product_id>/', views.ProductUpdateAPI.as_view(), name='update_descriptions'),
+    path('log/descriptions/<str:product_id>/', views.ProductListDescriptionsAPI.as_view(), name='list_descriptions'),
 
     path('test/', views.Test.as_view(), name='test'),
     path('oauth/callback/', views.oauth_callback, name='oauth_callback'),
