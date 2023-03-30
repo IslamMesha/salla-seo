8c215ae13bab67966e56fc3557dd4fe604fcc7ed
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-03-30 11:04:55 +0200

    redirect incase cookie has no meaning

--- app/authentication.py
+++ app/authentication.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.utils.translation import gettext_lazy as _
 
 from rest_framework.authentication import TokenAuthentication
@@ -18,7 +19,11 @@ class TokenAuthSupportCookie(TokenAuthentication):
         try:
             token = model.objects.get(public_token=key)
         except model.DoesNotExist:
-            raise exceptions.AuthenticationFailed(_('Invalid token.'))
+            response = redirect('app:index')
+            response.delete_cookie(CookieKeys.AUTH_TOKEN.value)
+            
+            return response
+            # raise exceptions.AuthenticationFailed(_('Invalid token.'))
 
         if not token.user.is_active:
             raise exceptions.AuthenticationFailed(_('User inactive or deleted.'))
