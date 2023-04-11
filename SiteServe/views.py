a904b22f8c1312220db0733807dd241cbfad9c1b
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-04-11 09:33:37 +0200

    view to open static pages

--- SiteServe/views.py
+++ SiteServe/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+from django.shortcuts import get_object_or_404
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.renderers import TemplateHTMLRenderer
+
+from SiteServe import models
+
+
+class LoadStaticPageView(APIView):
+    permission_classes = []
+    renderer_classes = [TemplateHTMLRenderer]
+
+    def get(self, request, slug):
+        page = get_object_or_404(models.StaticPage, slug=slug)
+        context = { 'page': page }
+
+        return Response(context, template_name='static_page.html')
+
+
+
