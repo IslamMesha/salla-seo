b0c54cdb2626ab3bf6bb06093757457c8b8a6e7f
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-04-15 02:04:47 +0200

    Enum Event for webhooks

--- app/enums.py
+++ app/enums.py
@@ -5,3 +5,8 @@ class CookieKeys(Enum):
     AUTH_TOKEN = 'auth_token'
 
 
+class WebhookEvents(Enum):
+    # hold names of the webhook events
+    AUTHORIZED = 'app.store.authorize'
+    SETTINGS_UPDATED = 'app.settings.updated'
+
