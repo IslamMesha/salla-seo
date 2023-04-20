9710925e230fd1df957ee0897a1d13c4dfe4fab9
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-04-20 10:41:46 +0200

    define static urls

--- dj/urls.py
+++ dj/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 urlpatterns = [
     # apps
@@ -10,3 +12,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
