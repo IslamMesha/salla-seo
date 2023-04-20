b5d3e9f19665fc71dd8a142fc92e2fa84e03af86
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-04-20 10:42:13 +0200

    add general context data

--- SiteServe/views.py
+++ SiteServe/views.py
@@ -15,9 +15,12 @@ class LoadStaticPageView(APIView):
 
     def get(self, request, slug):
         page = get_object_or_404(models.StaticPage, slug=slug)
+        app_id = os.environ.get('SALLA_APP_ID')
         context = {
+            'installation_url': f'https://s.salla.sa/apps/install/{app_id}',
             'page': page,
-            'nav_pages': models.StaticPage.get_nav_pages()
+            'nav_pages': models.StaticPage.get_nav_pages(),
+            'is_authenticated': request.user.is_authenticated,
         }
 
         return Response(context, template_name='static_page.html')
