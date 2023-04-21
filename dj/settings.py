53d79de5f0993c9ae810bd4ef12113ba8f768571
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-04-21 19:29:20 +0200

    some changes

--- dj/settings.py
+++ dj/settings.py
@@ -90,9 +90,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")] #in settings.py
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")] #in settings.py
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
