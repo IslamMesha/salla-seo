9a25f73fc729c815f1cb88542701dfd138adee88
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-04-21 19:02:42 +0200

    add loading page

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,8 @@ const products = document.querySelectorAll('.product')
 products.forEach(product => {
     product.setAttribute('dir', 'auto')
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loadingPage = document.querySelector('#loading-page')
+    loadingPage.remove();
+});
