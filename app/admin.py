e1ae954e65b114eb0ea1c258e87bf6b1a734add1
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-05-09 08:09:18 +0300

    register logs models to admin

--- app/admin.py
+++ app/admin.py
@@ -7,4 +7,5 @@ admin.site.register(models.SallaStore)
 admin.site.register(models.Account)
 admin.site.register(models.ChatGPTResponse)
 admin.site.register(models.UserPrompt)
+admin.site.register(models.SallaWebhookLog)
 
