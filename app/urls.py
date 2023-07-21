8e3d96214b643e8ae8b64687a3b8d34f76f9f948
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-07-21 18:22:01 +0300

    stop setting validation url

--- app/urls.py
+++ app/urls.py
@@ -19,6 +19,6 @@ urlpatterns = [
 
     path('oauth/callback/', views.oauth_callback, name='oauth_callback'),
     path('webhook/', views.WebhookAPI.as_view(), name='webhook'),
-    path('settings/validation/', views.SettingsValidationAPI.as_view(), name='settings_validation'),
+    # path('settings/validation/', views.SettingsValidationAPI.as_view(), name='settings_validation'),
     
 ]
