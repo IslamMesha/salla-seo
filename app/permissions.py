451d53fd921ab206f36a6fcfe7be3fbde676467c
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-09-07 18:29:05 +0300

    use the permission in the update

--- app/permissions.py
+++ app/permissions.py
@@ -5,14 +5,17 @@ class SallaPlanLimits(permissions.BasePermission):
     message = 'You have reached your plan limits'
 
     def has_permission(self, request, view):
+        from app.models import UserPrompt
+
         if request.user.is_authenticated:
             subscription = request.user.subscriptions.filter(is_active=True).last()
             if subscription and subscription.is_alive:
                 start_date = subscription.created_at
                 end_date = subscription.created_at + (subscription.plan_period or timedelta(days=1000)) 
                 # get prompts count during the subscription month
-                prompts_count = request.user.prompts.filter(
-                    created_at__gte=start_date, created_at__lte=end_date
-                ).count()
+                prompts_count = UserPrompt.count_for_user(request.user, start_date, end_date, gpt=True)
+                # request.user.prompts.filter(
+                #     created_at__gte=start_date, created_at__lte=end_date
+                # ).count()
                 return prompts_count < subscription.gpt_prompts_limit
         return False
