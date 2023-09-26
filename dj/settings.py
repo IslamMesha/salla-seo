f4ba74fd31d0e9270aafc0872b4493e48f43827d
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-09-26 21:57:02 +0300

    read email host from env file

--- dj/settings.py
+++ dj/settings.py
@@ -151,7 +151,7 @@ LOGGING = {
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
