e0fe7432c28c6f96e89748eac205bf6c84aea45f
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-10-18 04:21:17 +0300

    skip all brand name

--- SiteServe/models.py
+++ SiteServe/models.py
@@ -28,7 +28,7 @@ def ALLOWED_TEMPLATE_LABELS():
         '{product_description}',
         '{product_seo_title}',
         '{keywords}',
-        '{brand_name}',
+        # '{brand_name}',
     ]
     return readable_list(labels, 'or')
 
